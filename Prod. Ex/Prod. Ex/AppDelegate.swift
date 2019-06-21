105327ee208ab650f337290cc37f82c9613e6d73
albertjgonzalez <ajgonzalez1114@gmail.com>
2019-06-21 12:57:51 -0400

    started app

---
+++ Prod. Ex/Prod. Ex/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Prod. Ex
+//
+//  Created by Albert Jeremy Gonzalez on 6/21/19.
+//  Copyright © 2019 Albert Jeremy Gonzalez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Prod. Ex/Prod. Ex/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Prod. Ex
+//
+//  Created by Albert Jeremy Gonzalez on 6/21/19.
+//  Copyright © 2019 Albert Jeremy Gonzalez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
